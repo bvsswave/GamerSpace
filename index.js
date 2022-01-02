7170d7aa7b83a63da4091d761c2377352b8e4698
JakeRoberts99 <jake.s.roberts99@gmail.com>
2022-01-02 15:00:25 -0700

    deleted html and css stuff for a fresh start and added handlebars trying to figure out what isn't working

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const exphbs = require("expres-handlebars")
+const app = express();
+
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+server.engine('handlbars', exphbs({ defaultLayout: 'main'}));
+server.set('view engine', 'handlebars');
+
+require('./routes/api-routes.js')(app);
+require("./routes/html-routes.js")(app);
+
+//THIS came from socket.io, commented out to see if handlebars will work
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  });
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
+});
+
+//TODO - need models folder with index.js, and users.js
+//need to require models folder here example: const db - require('./models')
+//instead of server.listen you need a db.sequelize.sync().then(() => {
+// server.listen 
+// })
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
